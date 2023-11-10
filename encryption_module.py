67581c54095aeec9556ff19043440af06cd634e9
dookofcrds <43824616+dookofcrds@users.noreply.github.com>
2023-11-10 19:00:07 +0000

    Create encryption_module.py

---
+++ encryption_module.py
@@ -0,0 +1,11 @@
+# encryption_module.py
+
+def encrypt_data(data):
+    # Placeholder logic for encryption
+    encrypted_data = data[::-1]  # Reversing the data as a simple encryption method
+    return encrypted_data
+
+def decrypt_data(encrypted_data):
+    # Placeholder logic for decryption
+    decrypted_data = encrypted_data[::-1]  # Reversing the encrypted data to decrypt
+    return decrypted_data
